3327126c4a9c48182d913b60d6a530e8eced4132
hanifhuzaifa <hanifhuzaifa307@gmail.com>
2024-11-06 10:51:32 -0800

    master

---
+++ chapter12-13.js
@@ -0,0 +1,95 @@
+// // // // 1.
+// // // let character = prompt("Enter a single character:");
+
+// // // if (character.length === 1) {
+// // //     let asciiValue = character.charCodeAt(0);
+
+// // //     if (asciiValue >= 48 && asciiValue <= 57) {
+// // //         console.log("The character is a number.");
+// // //     }
+// // //     else if (asciiValue >= 65 && asciiValue <= 90) {
+// // //         console.log("The character is an uppercase letter.");
+// // //     }
+// // //     else if (asciiValue >= 97 && asciiValue <= 122) {
+// // //         console.log("The character is a lowercase letter.");
+// // //     }
+// // //     else {
+// // //         console.log("The character is a special symbol or something else.");
+// // //     }
+// // // } else {
+// // //     console.log("Please enter only a single character.");
+// // // }
+
+// // // 2.
+
+// // let num1 = +prompt("Enter a integers:");
+// // let num2 = +prompt("Enter a integers:");
+
+// // if (num1 > num2) {
+// //     console.log("The larger number is: " + num1);
+// // } else if (num2 > num1) {
+// //     console.log("The larger number is: " + num2);
+// // } else {
+// //     console.log("Both numbers are equal.");
+// // }
+
+// // // 3.
+
+// // let num = +prompt("Enter a num:");
+
+// // if (num > 0) {
+// //     console.log("The number is positive: " + num);
+// // } else if (num < 0) {
+// //     console.log("The number is nagitive: " + num);
+// // } else {
+// //     console.log(" numbers is 0.");
+// // }
+
+// // // 4.
+
+// // let char = prompt("Enter a latter");
+
+// // if (char.length === 1) {
+// //   let latter = char.toLowerCase();
+
+// //   if (
+// //     latter == "a" ||
+// //     latter == "e" ||
+// //     latter == "i" ||
+// //     latter == "o" ||
+// //     latter == "u"
+// //   ) {
+// //     console.log(true);
+// //   } else {
+// //     console.log(false);
+// //   }
+// // }  else {
+// //     console.log("Please enter only a single character.");
+// // }
+
+
+// //  5.
+
+// let pass = "huz123"
+// let confirmationpass = prompt("Confirm Password")
+
+// if (pass == confirmationpass) {
+//     console.log("Correct! The password you entered matches the original password");
+    
+// }else{
+//     console.log("Incorrect password");
+    
+// }
+
+
+// //  6.
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+//     greeting = "Good day";
+// } else {
+//     greeting = "Good evening";
+// }
+
+// console.log(greeting);  
---
+++ chapter14-16.js
@@ -0,0 +1,76 @@
+// 1.
+
+// // // // let student = [];
+// // // // let count = +prompt("Enter student count");
+
+// // // // for (let i = 0; i < count; i++) {
+// // // //   let emp = prompt("Enter student Name");
+// // // //   student.push(emp);
+// // // // }
+// // // // console.log(student);
+
+// // // // 2.
+// // // let fruits = ["Apple", "Banana", "Cherry", "Date"];
+// // // console.log(fruits[0]);  // Output: "Apple"
+// // // console.log(fruits[2]);  // Output: "Cherry"
+
+// // // 3.
+// // let num = [1,2,3,4,5];
+// // console.log(num[0]);  // Output: "Apple"
+// // console.log(num[2]);  // Output: "Cherry"
+
+// // 4.
+// // let boolArray = [true, false, true, false];
+// // console.log(boolArray[0]);
+// // console.log(boolArray[1]);
+
+// // 5.
+// // let mixedArray = [42, "Hello", true, { name: "John", age: 30 }, [1, 2, 3]];
+// // console.log(mixedArray[0]);
+// // console.log(mixedArray[1]);
+// // console.log(mixedArray[3]);
+// // console.log(mixedArray[4][1]);
+
+// // 6.
+
+// // let studentName = [];
+// // let Score = [];
+// // for (let i = 0; i < 3; i++) {
+// //   let name = prompt("Enter 3 student name...");
+// //   studentName.push(name);
+// //   let takeScore = +prompt("Enter 3 student Score...");
+// //   Score.push(takeScore);
+// // }
+// // console.log("Score of ", studentName[0] , "is " , Score[0]);
+// // console.log("Score of ", studentName[1] , "is " , Score[1]);
+// // console.log("Score of ", studentName[2] , "is " , Score[2]);
+
+// // 7.
+
+// // let arr = [454,234,654,454,236,675]
+
+// // arr.sort()
+// // console.log(arr);
+
+// // 8.
+// // let city= ["karachi", "lahore", "peshawer","quetta", "isl"]
+
+// // console.log(city.slice(2,4));
+
+// // 12.
+
+// var arr = ["This", "is", "my", "cat"];
+
+// console.log(arr.join(" "));
+
+// 15
+
+// let phoneManufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+// document.write("<select>");
+
+// for (let i = 0; i < phoneManufacturers.length; i++) {
+//   document.write("<option>" + phoneManufacturers[i] + "</option>");
+// }
+
+// document.write("</select>");
