46780ea4045de9de22b15347ca3909e29b8fb57d
hanifhuzaifa <hanifhuzaifa307@gmail.com>
2024-11-16 01:23:20 -0800

    master

---
+++ chapter21-25.js
@@ -0,0 +1,42 @@
+// // 1.
+// let firstName = prompt("Enter First Name")
+// let LAstName = prompt("Enter last Name")
+
+// let fullname = ''
+
+// fullname = firstName + LAstName
+// console.log("Salam :" ,fullname);
+
+// 2.
+//  let inp = prompt("Enter favorite mobile phone model.")
+
+//  console.log("length of string is : ", inp.length);
+
+// 3.
+
+// let word = prompt("Enter the word of pakistani..!");
+
+// console.log("String is : ", word);
+
+// console.log("index of n is :", word.indexOf("n"));
+
+
+// 4.
+
+// let word = prompt("Enter the word of hello world..!");
+
+// console.log("String is : ", word);
+
+// console.log("index of l is :", word.lastIndexOf("l"));
+
+
+
+
+// 3.
+
+// let word = prompt("Enter the word of pakistani..!");
+
+// console.log("String is : ", word);
+
+// console.log("index of i is :", word.charAt(3));
+
