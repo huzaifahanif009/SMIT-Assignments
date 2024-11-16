952fa380433852f33053abc18536d12ddd516164
hanifhuzaifa <hanifhuzaifa307@gmail.com>
2024-11-16 00:46:09 -0800

    master

--- chapter17-20.js
+++ chapter17-20.js
@@ -1,2 +1,143 @@
-// let multiArray = [[], []];
+// // let multiArray = [[], []];
+// print("hello world")
 
+// 2.
+// const matrix = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+
+// console.log(matrix);
+// console.log(matrix[1][2]);
+
+// 3.
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// 4.
+
+// let tableNumber = +prompt("Enter number to print table");
+// let length = +prompt("Enter Length...");
+
+// for (let i = 1; i <= length; i++) {
+//   console.log(`${tableNumber} * ${i} = ${tableNumber * i}`);
+// }
+
+// 5.
+// let fruits = ["“apple”", "“banana”", "“mango”", "“orange”", "“strawberry”"];
+
+// for (let p = 0; p < fruits.length; p++) {
+//     console.log(fruits[p]);
+
+// }
+
+// 6.
+// let counting = [];
+// for (let i = 1; i <= 15; i++) {
+//     console.log(counting.push(i));
+
+// }
+
+// Reverse counting:
+// for (let i = 10; i >= 1; i--) {
+//     console.log("Reverse counting:",i);
+
+// }
+
+// Generate and display Even Numbers using "%"
+// let evenNumbers = [];
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         evenNumbers.push(i);
+//     }
+// }
+// console.log("Even:", evenNumbers.join(", "));
+
+// // Generate and display Even Numbers using "%"
+// let oddNumbers = [];
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 === 1) {
+//         oddNumbers.push(i);
+//     }
+// }
+// console.log("odd:", oddNumbers.join(", "));
+
+// let evenNumbers = [];
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         evenNumbers.push(i);
+//     }
+// }
+// console.log("Even:", evenNumbers.join("K, "));
+
+
+
+
+// 7.
+
+// let Item = ["cake", "apple pie", "cookie", "chips", "patties"];
+// let search = prompt("Find the item in bakery").toLowerCase();
+
+// let found = false;
+
+// for (let i = 0; i <= Item.length; i++) {
+//   if (search == Item[i]) {
+//     found = true;
+//     break;
+//   }
+// }
+
+// if (found) {
+//   console.log(`${search} is Avalible in our bakery`);
+// } else {
+//   console.log(`We are sorry, ${search} is not avalible in our bakery`);
+// }
+
+
+
+
+
+
+// // 8.
+
+// let A = [24, 53, 78, 91, 12]
+
+// let largest = A[0]
+
+// for (let i = 0; i < A.length; i++) {
+// if (A[i] > largest) {
+//     largest = A[i]
+// }    
+// }
+
+// console.log("The largest number is :",largest);
+
+
+
+
+
+// 9.
+
+// let A = [24, 53, 78, 91, 12]
+
+// let smallest = A[0]
+
+// for (let i = 0; i < A.length; i++) {
+// if (A[i] < smallest) {
+//     smallest = A[i]
+// }    
+// }
+
+// console.log("The smallest number is :",smallest);
+
+
+
+// 10.
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
